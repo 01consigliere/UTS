d8585b99cb030f89a1ceb0d349497c0071e7877a
01consigliere <anan.realmeXT@gmail.com>
2024-10-25 10:22:16 +0700

    Update lingkaran_anan.py

--- lingkaran_anan.py
+++ lingkaran_anan.py
@@ -47,7 +47,5 @@ st.pyplot(fig)
 if button:
     fig = plot_data()
     st.write('Gambarnya Berubah kan? Coba Tekan Lagi')
-    if button:
-        fig = plot_data()
-        st.write('Berubah Lagi Kan, Selamat Menikmati yaa')
+
 
